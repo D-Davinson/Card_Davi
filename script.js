6c870dc3d1842b89afec777af906b2a3e07449a5
Davinson <doglasprincedavinson@gmail.com>
2022-12-22 15:19:15 +0100

    optimisation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // instragram
 const l1 = document.querySelectorAll("a1");
 const l1Array = [...l1];
-l1Array.map(lien => lien.addEventListener("click", function(event){
+l1Array.map(lien => lien.addEventListener("click", (event) => {
     event.preventDefault();
     location.href="https://www.instagram.com/d_davi30/"
 }));
@@ -11,7 +11,7 @@ l1Array.map(lien => lien.addEventListener("click", function(event){
 
 const l2 = document.querySelectorAll("a2");
 const l2Array = [...l2];
-l2Array.map(lien => lien.addEventListener("click", function(event){
+l2Array.map(lien => lien.addEventListener("click", (event)=> {
     event.preventDefault();
     location.href="https://github.com/D-Davinson"
 }));
@@ -20,7 +20,7 @@ l2Array.map(lien => lien.addEventListener("click", function(event){
 //linkedin
 const l3 = document.querySelectorAll("a3");
 const l3Array = [...l3];
-l3Array.map(lien => lien.addEventListener("click", function(event){
+l3Array.map(lien => lien.addEventListener("click", (event) => {
     event.preventDefault();
     location.href="https://www.linkedin.com/in/davinson-doglas-prince-960ba8255/"
 }));
@@ -28,7 +28,7 @@ l3Array.map(lien => lien.addEventListener("click", function(event){
 //whatsapp
 const l4 = document.querySelectorAll("a4");
 const l4Array = [...l4];
-l4Array.map(lien => lien.addEventListener("click", function(event){
+l4Array.map(lien => lien.addEventListener("click", (event) => {
     event.preventDefault();
     alert("☎️ Tel : +33.7.67.35.60.02");
 }));
